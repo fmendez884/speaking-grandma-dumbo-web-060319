4e548019a6a7d83fe67106609f888e6d248ec0e4
Francis Mendez <fmendez884@gmail.com>
2019-04-08 18:16:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   elsif phrase != phrase.upcase
-    return "HUH!? SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
